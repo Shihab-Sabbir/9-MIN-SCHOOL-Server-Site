5a6715925ab43110cb9f5b4754695a21bc982ce2
shihab-sabbir <shihab11231@gmail.com>
2022-10-25 03:01:45 +0600

    pagination done with courses

--- index.js
+++ index.js
@@ -11,15 +11,7 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(data)
 })
-app.get('/courses/:page', (req, res) => {
-    let courses = data.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
-    let pageNumber = parseInt(req.params.page);
-    const start = 1 + (pageNumber - 1) * 4;
-    let end = 5 + (pageNumber - 1) * 4;
-    let limitedData = courses.slice(start - 1, end - 1);
-    const sendData = { pages: data.length, data: [...limitedData], start, end };
-    res.send(sendData);
-})
+
 
 app.get('/course/:id', (req, res) => {
     const course = data.find(item => (item.id == req.params.id));
