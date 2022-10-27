a130ee028435ca7631c55cde72ac702a3bc4898e
shihab-sabbir <shihab11231@gmail.com>
2022-10-27 16:38:32 +0600

    readme update

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Node-Express-Cors-Boiler_Plate
-# b610-lerning-platform-server-side-Shihab-Sabbir
-# b610-lerning-platform-server-side-Shihab-Sabbir
+# <span style="color:red;font-size:30px">9</span> <span style="font-size:20px">MIN SCH<span style="color:red;">OO</span>L</span>
+
+## <span style="font-size:15px">SERVER SITE</span>
+
+Server , [Live Here](https://9-min-school-3a4gh9rwz-shihab-sabbir.vercel.app/)
+
+> ###### Server is Deployed in Vercel
+
+## Features
+
+- Get API
+- Searching facility
+- Pagination Facility
+
+### `Notable Used Technology`
+
+- Node.Js
+- Express
+- Cors
--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.get('/courses', (req, res) => {
 app.get('/courses/:page', (req, res) => {
     let courses = data.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
     let pageNumber = parseInt(req.params.page);
-    const start = 1 + (pageNumber - 1) * 4;
-    let end = 5 + (pageNumber - 1) * 4;
+    const start = 1 + (pageNumber - 1) * 6;
+    let end = 7 + (pageNumber - 1) * 6;
     let limitedData = courses.slice(start - 1, end - 1);
     const sendData = { pages: data.length, data: [...limitedData], start, end };
     res.send(sendData);
@@ -23,6 +23,11 @@ app.get('/courses/:page', (req, res) => {
 
 app.get('/course/:id', (req, res) => {
     const course = data.find(item => (item.id == req.params.id));
+    res.send(course);
+})
+
+app.get('/search/:course_name', (req, res) => {
+    const course = data.filter(item => (item.name.toLowerCase()).includes(req.params.course_name.toLowerCase()) || (item.details.toLowerCase()).includes(req.params.course_name.toLowerCase()))
     res.send(course);
 })
 
